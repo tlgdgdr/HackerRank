3eb47701ecc464a81ac8e6f3320f17aadab7406e
Tolga Dagidir <33180476+tlgdgdr@users.noreply.github.com>
2023-09-02 23:55:58 +0200

    Create stringTokenizer.java

---
+++ stringTokenizer.java
@@ -0,0 +1,14 @@
+// String Tokenizer
+
+public static void main(String[] args) {
+  Scanner scan = new Scanner(System.in);
+  String s = scan.nextLine();
+  
+  StringTokenizer st = new StringTokenizer(s,"[, .?!@?._']"+" ");
+  System.out.println(st.countTokens());
+  while(st.hasMoreTokens()){
+      System.out.println(st.nextToken());
+  }
+  
+  scan.close();
+}
